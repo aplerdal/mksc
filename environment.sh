7addbb882c11e4031f642e3a7235d232cd733be1
Andrew Lerdal <59904070+aplerdal@users.noreply.github.com>
2024-10-28 15:57:30 +0000

    Add environment setup

---
+++ environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+
+# This script is for setting up environment variables
+
+export DEVKITPRO=/opt/devkitpro
+export DEVKITARM=/opt/devkitpro/devkitARM
+export PATH=$PATH:$DEVKITPRO/tools/bin
