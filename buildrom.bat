eec8210d95a6f480c8d9017b012377aea5ce58ff
Andrew Lerdal <59904070+aplerdal@users.noreply.github.com>
2025-05-15 16:33:23 +0000

    Add Docker installation warning

--- buildrom.bat
+++ buildrom.bat
@@ -1,3 +1,9 @@
 @echo off
 REM buildrom.bat
+where docker >nul 2>&1
+if errorlevel 1 (
+    echo Docker is not installed, please install it and try again.
+    PAUSE
+    exit /b 1
+)
 docker run --rm -v "%cd%":/project -w /project antimattur/mksc %*
